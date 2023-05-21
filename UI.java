eae1f453237f2942f46608a22fbd13872848fa5d
Dummdevka <87282400+Dummdevka@users.noreply.github.com>
2023-05-21 15:54:02 +0200

    changed buttonsContainer layout

--- UI.java
+++ UI.java
@@ -2,6 +2,8 @@ package learn_java.simple_player;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
@@ -36,12 +38,12 @@ public class UI implements ActionListener{
         //Panels
         PanelConstructor navbar = new PanelConstructor(500, 80, wine, null, new BorderLayout());
         PanelConstructor content = new PanelConstructor(500, 390, cadet, null, new BorderLayout());
-        PanelConstructor buttonsContainer = new PanelConstructor(150, 80, violet, BorderFactory.createMatteBorder(2, 2, 2, 2, wine), new BorderLayout());
+        PanelConstructor buttonsContainer = new PanelConstructor(150, 80, violet, BorderFactory.createMatteBorder(2, 2, 2, 2, wine), new FlowLayout(FlowLayout.CENTER, 5, 15));
         PanelConstructor progressBar = new PanelConstructor(400, 50, oxford, null, new BorderLayout());
         PanelConstructor footer = new PanelConstructor(500, 30, Color.black, null, new BorderLayout());
 
         content.setBorder(new EmptyBorder(100, 30, 60, 30));
-        buttonsContainer.setBorder(new EmptyBorder(10, 50, 10, 50));
+        // buttonsContainer.setBorder(new EmptyBorder(10, 50, 10, 50));
 
         content.add(buttonsContainer, BorderLayout.SOUTH);
         content.add(progressBar, BorderLayout.NORTH);
@@ -68,16 +70,15 @@ public class UI implements ActionListener{
         pauseIcon = new ImageIcon(pauseIcon.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH));
         resetIcon = new ImageIcon(resetIcon.getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH));
 
-        playBtn = new ButtonConstructor(50, 50, null, playIcon, Color.lightGray, BorderFactory.createBevelBorder(0));
+        playBtn = new ButtonConstructor(150, 50, null, playIcon, Color.lightGray, BorderFactory.createBevelBorder(0));
         pauseBtn = new ButtonConstructor(50, 50, null, pauseIcon, Color.lightGray, BorderFactory.createBevelBorder(0));
         resetBtn = new ButtonConstructor(50, 50, null, resetIcon, Color.lightGray, BorderFactory.createBevelBorder(0));
-
         playBtn.addActionListener(this);
         pauseBtn.addActionListener(this);
         resetBtn.addActionListener(this);
-        buttonsContainer.add(pauseBtn, BorderLayout.WEST);
-        buttonsContainer.add(playBtn, BorderLayout.CENTER);
-        buttonsContainer.add(resetBtn, BorderLayout.EAST);
+        buttonsContainer.add(pauseBtn);
+        buttonsContainer.add(playBtn);
+        buttonsContainer.add(resetBtn);
         //Frame
         FrameConstructor frame = new FrameConstructor(500, 500, Color.lightGray);
 
